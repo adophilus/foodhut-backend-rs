63e4108d9b8c7f0b26fe59e834057756906ddb09
Adophilus <uchenna19of@gmail.com>
2024-10-23 18:37:25 +0100

    feat: added support for searching for meals

--- src/repository/meal.rs
+++ src/repository/meal.rs
@@ -1,5 +1,5 @@
-use crate::utils;
 use crate::utils::storage::UploadedMedia;
+use crate::{define_paginated, utils};
 use bigdecimal::FromPrimitive;
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
@@ -51,6 +51,8 @@ impl ToString for MealUserReactionReaction {
         }
     }
 }
+
+define_paginated!(DatabasePaginatedMeal, Meal);
 
 pub struct CreateMealPayload {
     pub name: String,
@@ -152,8 +154,8 @@ struct DatabaseCounted {
 
 #[derive(Deserialize)]
 pub struct Filters {
-    kitchen_id: Option<String>,
-    search: Option<String>,
+    pub kitchen_id: Option<String>,
+    pub search: Option<String>,
 }
 
 pub async fn find_many(
@@ -161,50 +163,46 @@ pub async fn find_many(
     pagination: Pagination,
     filters: Filters,
 ) -> Result<Paginated<Meal>, Error> {
-    match sqlx::query_as!(
-        DatabaseCounted,
-        "
+    sqlx::query_as!(
+        DatabasePaginatedMeal,
+        r#"
             WITH filtered_data AS (
                 SELECT *
-                FROM meals 
-                WHERE
-                    kitchen_id = COALESCE($3, kitchen_id)
-                    AND name ILIKE CONCAT('%', COALESCE($4, name), '%')
-                LIMIT $1
-                OFFSET $2
-            ), 
-            total_count AS (
-                SELECT COUNT(id) AS total_rows
                 FROM meals
                 WHERE
-                    kitchen_id = COALESCE($3, kitchen_id)
-                    AND name ILIKE CONCAT('%', COALESCE($4, name), '%')
+                    ($3::TEXT IS NULL OR meals.kitchen_id = $3)
+                    AND ($4::TEXT IS NULL OR meals.name ILIKE CONCAT('%', $4, '%'))
+                LIMIT $1
+                OFFSET $2
+            ),
+            total_count AS (
+                SELECT COUNT(*) AS total_rows
+                FROM meals
+                WHERE
+                    ($3::TEXT IS NULL OR meals.kitchen_id = $3)
+                    AND ($4::TEXT IS NULL OR meals.name ILIKE CONCAT('%', $4, '%'))
             )
-            SELECT JSONB_BUILD_OBJECT(
-                'data', COALESCE(JSONB_AGG(ROW_TO_JSON(filtered_data)), '[]'::jsonb),
-                'total', (SELECT total_rows FROM total_count)
-            ) AS result
+            SELECT
+                COALESCE(JSONB_AGG(ROW_TO_JSON(filtered_data)), '[]'::jsonb) AS items,
+                JSONB_BUILD_OBJECT(
+                    'total', (SELECT total_rows FROM total_count),
+                    'per_page', $1,
+                    'page', $2 / $1 + 1
+                ) AS meta
             FROM filtered_data;
-        ",
+        "#,
         pagination.per_page as i64,
         ((pagination.page - 1) * pagination.per_page) as i64,
         filters.kitchen_id,
-        filters.search,
+        filters.search
     )
     .fetch_one(&db.pool)
     .await
-    {
-        Ok(counted) => Ok(Paginated::new(
-            counted.result.data,
-            counted.result.total,
-            pagination.page,
-            pagination.per_page,
-        )),
-        Err(err) => {
-            tracing::error!("Error occurred while trying to fetch many meals: {}", err);
-            Err(Error::UnexpectedError)
-        }
-    }
+    .map(DatabasePaginatedMeal::into)
+    .map_err(|err| {
+        tracing::error!("Error occurred while trying to fetch many meals: {}", err);
+        Error::UnexpectedError
+    })
 }
 
 #[derive(Serialize)]
