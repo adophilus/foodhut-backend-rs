f716f5a1d1d3a639abf774795faad38dfec41b29
Adophilus <uchenna19of@gmail.com>
2025-01-28 16:37:41 +0100

    feat: made the meals endpoint to work (for viewing) without authentication

--- src/modules/meal/routes.rs
+++ src/modules/meal/routes.rs
@@ -145,91 +145,245 @@ struct Filters {
 
 async fn get_meals(
     State(ctx): State<Arc<Context>>,
-    auth: Auth,
+    maybe_auth: Option<Auth>,
     pagination: Pagination,
     Query(filters): Query<Filters>,
 ) -> impl IntoResponse {
-    let is_liked_by = match filters.is_liked {
-        Some(true) => Some(auth.user.id.clone()),
-        _ => None,
-    };
-
-    let cart = match cart::repository::find_active_cart_by_owner_id(
-        &ctx.db_conn.pool,
-        auth.user.id.clone(),
-    )
-    .await
-    {
-        Ok(Some(cart)) => cart,
-        Ok(None) => {
-            match cart::repository::create(
+    match maybe_auth {
+        Some(auth) => {
+            let cart = match cart::repository::find_active_cart_by_owner_id(
                 &ctx.db_conn.pool,
-                cart::repository::CreateCartPayload {
-                    owner_id: auth.user.id.clone(),
-                },
+                auth.user.id.clone(),
             )
             .await
             {
-                Ok(cart) => cart,
+                Ok(Some(cart)) => cart,
+                Ok(None) => {
+                    match cart::repository::create(
+                        &ctx.db_conn.pool,
+                        cart::repository::CreateCartPayload {
+                            owner_id: auth.user.id.clone(),
+                        },
+                    )
+                    .await
+                    {
+                        Ok(cart) => cart,
+                        Err(_) => {
+                            return (
+                                StatusCode::INTERNAL_SERVER_ERROR,
+                                Json(json!({"error": "Failed to fetch meals"})),
+                            )
+                        }
+                    }
+                }
                 Err(_) => {
                     return (
                         StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!({"error": "Failed to fetch meals"})),
+                        Json(json!({ "error": "Failed to fetch meals"})),
                     )
                 }
+            };
+
+            let is_liked_by = match filters.is_liked {
+                Some(true) => Some(auth.user.id.clone()),
+                _ => None,
+            };
+
+            let paginated_meals = if user::repository::is_admin(&auth.user) {
+                repository::find_many_as_admin(
+                    &ctx.db_conn.pool,
+                    pagination.clone(),
+                    repository::FindManyAsAdminFilters {
+                        kitchen_id: filters.kitchen_id,
+                        search: filters.search,
+                        is_liked_by,
+                    },
+                )
+                .await
+            } else if filters.as_kitchen.unwrap_or(false) {
+                repository::find_many_as_kitchen(
+                    &ctx.db_conn.pool,
+                    pagination.clone(),
+                    repository::FindManyAsKitchenFilters {
+                        kitchen_id: filters.kitchen_id,
+                        search: filters.search,
+                        is_liked_by,
+                        owner_id: auth.user.id.clone(),
+                    },
+                )
+                .await
+            } else {
+                repository::find_many_as_user(
+                    &ctx.db_conn.pool,
+                    pagination.clone(),
+                    repository::FindManyAsUserFilters {
+                        kitchen_id: filters.kitchen_id,
+                        search: filters.search,
+                        is_liked_by,
+                    },
+                )
+                .await
+            };
+
+            match paginated_meals {
+                Ok(paginated_meals) => {
+                    let augmented_meals = paginated_meals
+                        .items
+                        .clone()
+                        .into_iter()
+                        .map(|meal| MealWithCartStatus {
+                            id: meal.id.clone(),
+                            name: meal.name,
+                            description: meal.description,
+                            rating: meal.rating,
+                            original_price: meal.original_price,
+                            price: meal.price,
+                            likes: meal.likes,
+                            cover_image: meal.cover_image,
+                            is_available: meal.is_available,
+                            in_cart: cart
+                                .items
+                                .0
+                                .iter()
+                                .find(|item| item.meal_id == meal.id)
+                                .is_some(),
+                            kitchen_id: meal.kitchen_id,
+                            created_at: meal.created_at,
+                            updated_at: meal.updated_at,
+                            deleted_at: meal.deleted_at,
+                        })
+                        .collect::<Vec<_>>();
+
+                    (
+                        StatusCode::OK,
+                        Json(json!(pagination::Paginated::new(
+                            augmented_meals,
+                            paginated_meals.meta.total,
+                            paginated_meals.meta.page,
+                            paginated_meals.meta.per_page,
+                        ))),
+                    )
+                }
+                _ => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(json!({"error": "Failed to fetch meals"})),
+                ),
             }
+        }
+        None => {
+            let paginated_meals = repository::find_many_as_user(
+                &ctx.db_conn.pool,
+                pagination.clone(),
+                repository::FindManyAsUserFilters {
+                    kitchen_id: filters.kitchen_id,
+                    search: filters.search,
+                    is_liked_by: None,
+                },
+            )
+            .await;
+
+            match paginated_meals {
+                Ok(paginated_meals) => {
+                    let augmented_meals = paginated_meals
+                        .items
+                        .clone()
+                        .into_iter()
+                        .map(|meal| MealWithCartStatus {
+                            id: meal.id.clone(),
+                            name: meal.name,
+                            description: meal.description,
+                            rating: meal.rating,
+                            original_price: meal.original_price,
+                            price: meal.price,
+                            likes: meal.likes,
+                            cover_image: meal.cover_image,
+                            is_available: meal.is_available,
+                            in_cart: false,
+                            kitchen_id: meal.kitchen_id,
+                            created_at: meal.created_at,
+                            updated_at: meal.updated_at,
+                            deleted_at: meal.deleted_at,
+                        })
+                        .collect::<Vec<_>>();
+
+                    (
+                        StatusCode::OK,
+                        Json(json!(pagination::Paginated::new(
+                            augmented_meals,
+                            paginated_meals.meta.total,
+                            paginated_meals.meta.page,
+                            paginated_meals.meta.per_page,
+                        ))),
+                    )
+                }
+                _ => (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    Json(json!({"error": "Failed to fetch meals"})),
+                ),
+            }
+        }
+    }
+}
+
+async fn get_meal_by_id(
+    Path(id): Path<String>,
+    State(ctx): State<Arc<Context>>,
+    maybe_auth: Option<Auth>,
+) -> impl IntoResponse {
+    let meal = match repository::find_by_id(&ctx.db_conn.pool, id).await {
+        Ok(Some(meal)) => meal,
+        Ok(None) => {
+            return (
+                StatusCode::NOT_FOUND,
+                Json(json!({ "error": "Meal not found" })),
+            )
         }
         Err(_) => {
             return (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                Json(json!({ "error": "Failed to fetch meals"})),
+                Json(json!({"error": "Failed to fetch meals"})),
             )
         }
     };
 
-    let meals_result = if user::repository::is_admin(&auth.user) {
-        repository::find_many_as_admin(
-            &ctx.db_conn.pool,
-            pagination.clone(),
-            repository::FindManyAsAdminFilters {
-                kitchen_id: filters.kitchen_id,
-                search: filters.search,
-                is_liked_by,
-            },
-        )
-        .await
-    } else if filters.as_kitchen.unwrap_or(false) {
-        repository::find_many_as_kitchen(
-            &ctx.db_conn.pool,
-            pagination.clone(),
-            repository::FindManyAsKitchenFilters {
-                kitchen_id: filters.kitchen_id,
-                search: filters.search,
-                is_liked_by,
-                owner_id: auth.user.id.clone(),
-            },
-        )
-        .await
-    } else {
-        repository::find_many_as_user(
-            &ctx.db_conn.pool,
-            pagination.clone(),
-            repository::FindManyAsUserFilters {
-                kitchen_id: filters.kitchen_id,
-                search: filters.search,
-                is_liked_by,
-            },
-        )
-        .await
-    };
+    match maybe_auth {
+        Some(auth) => {
+            let cart = match cart::repository::find_active_cart_by_owner_id(
+                &ctx.db_conn.pool,
+                auth.user.id.clone(),
+            )
+            .await
+            {
+                Ok(Some(cart)) => cart,
+                Ok(None) => {
+                    match cart::repository::create(
+                        &ctx.db_conn.pool,
+                        CreateCartPayload {
+                            owner_id: auth.user.id.clone(),
+                        },
+                    )
+                    .await
+                    {
+                        Ok(cart) => cart,
+                        Err(_) => {
+                            return (
+                                StatusCode::INTERNAL_SERVER_ERROR,
+                                Json(json!({"error": "Failed to fetch meals"})),
+                            )
+                        }
+                    }
+                }
+                Err(_) => {
+                    return (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        Json(json!({ "error": "Failed to fetch meals"})),
+                    )
+                }
+            };
 
-    match meals_result {
-        Ok(paginated_meals) => {
-            let augmented_meals = paginated_meals
-                .items
-                .clone()
-                .into_iter()
-                .map(|meal| MealWithCartStatus {
+            (
+                StatusCode::OK,
+                Json(json!(MealWithCartStatus {
                     id: meal.id.clone(),
                     name: meal.name,
                     description: meal.description,
@@ -249,67 +403,12 @@ async fn get_meals(
                     created_at: meal.created_at,
                     updated_at: meal.updated_at,
                     deleted_at: meal.deleted_at,
-                })
-                .collect::<Vec<_>>();
-
-            (
-                StatusCode::OK,
-                Json(json!(pagination::Paginated::new(
-                    augmented_meals,
-                    paginated_meals.meta.total,
-                    paginated_meals.meta.page,
-                    paginated_meals.meta.per_page,
-                ))),
+                })),
             )
         }
-        Err(_) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({"error": "Failed to fetch meals"})),
-        ),
-    }
-}
-
-async fn get_meal_by_id(
-    Path(id): Path<String>,
-    State(ctx): State<Arc<Context>>,
-    auth: Auth,
-) -> impl IntoResponse {
-    let cart = match cart::repository::find_active_cart_by_owner_id(
-        &ctx.db_conn.pool,
-        auth.user.id.clone(),
-    )
-    .await
-    {
-        Ok(Some(cart)) => cart,
-        Ok(None) => {
-            match cart::repository::create(
-                &ctx.db_conn.pool,
-                CreateCartPayload {
-                    owner_id: auth.user.id.clone(),
-                },
-            )
-            .await
-            {
-                Ok(cart) => cart,
-                Err(_) => {
-                    return (
-                        StatusCode::INTERNAL_SERVER_ERROR,
-                        Json(json!({"error": "Failed to fetch meals"})),
-                    )
-                }
-            }
-        }
-        Err(_) => {
-            return (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                Json(json!({ "error": "Failed to fetch meals"})),
-            )
-        }
-    };
-
-    match repository::find_by_id(&ctx.db_conn.pool, id).await {
-        Ok(Some(meal)) => {
-            let augmented_meal = MealWithCartStatus {
+        _ => (
+            StatusCode::OK,
+            Json(json!(MealWithCartStatus {
                 id: meal.id.clone(),
                 name: meal.name,
                 description: meal.description,
@@ -319,27 +418,12 @@ async fn get_meal_by_id(
                 likes: meal.likes,
                 cover_image: meal.cover_image,
                 is_available: meal.is_available,
-                in_cart: cart
-                    .items
-                    .0
-                    .iter()
-                    .find(|item| item.meal_id == meal.id)
-                    .is_some(),
+                in_cart: false,
                 kitchen_id: meal.kitchen_id,
                 created_at: meal.created_at,
                 updated_at: meal.updated_at,
                 deleted_at: meal.deleted_at,
-            };
-
-            (StatusCode::OK, Json(json!(augmented_meal)))
-        }
-        Ok(None) => (
-            StatusCode::NOT_FOUND,
-            Json(json!({ "error": "Meal not found" })),
-        ),
-        Err(_) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({"error": "Failed to fetch meals"})),
+            })),
         ),
     }
 }
