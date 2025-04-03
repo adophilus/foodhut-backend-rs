1d99cf0f2b07cf4354a21edd80c15e85ab7d3714
Adophilus <uchenna19of@gmail.com>
2025-04-03 09:52:32 +0100

    fix: corrected type errors in the service

--- src/modules/ad/routes/list/service.rs
+++ src/modules/ad/routes/list/service.rs
@@ -1,13 +1,9 @@
 use super::types::{request, response};
-use crate::{
-    modules::{ad::repository, storage},
-    types::Context,
-};
-use std::io::Read;
+use crate::{modules::ad::repository, types::Context};
 use std::sync::Arc;
 
 pub async fn service(ctx: Arc<Context>, payload: request::Payload) -> response::Response {
-    repository::find_many(&ctx.db_conn.pool, pagination.clone(), filters)
+    repository::find_many(&ctx.db_conn.pool, payload.pagination, payload.filters)
         .await
         .map(response::Success::PaginatedAds)
         .map_err(|_| response::Error::FailedToFetchAds)
--- src/modules/ad/routes/mod.rs
+++ src/modules/ad/routes/mod.rs
@@ -1,9 +1,12 @@
 mod create;
+mod list;
 
 use crate::types::Context;
 use axum::routing::Router;
 use std::sync::Arc;
 
 pub fn get_router() -> Router<Arc<Context>> {
-    Router::new().nest("/", create::get_router())
+    Router::new()
+        .nest("/", create::get_router())
+        .nest("/", list::get_router())
 }
