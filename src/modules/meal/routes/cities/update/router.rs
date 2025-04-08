5d8c648a4f21b26e550d9d5f315a1352e17e44f2
Adophilus <uchenna19of@gmail.com>
2025-04-08 19:14:04 +0100

    refactor: removed the block and cities routes for meals

--- src/modules/meal/routes/cities/update/router.rs
+++
@@ -1,8 +0,0 @@
-use super::handler;
-use crate::types::Context;
-use axum::routing::{patch, Router};
-use std::sync::Arc;
-
-pub fn get_router() -> Router<Arc<Context>> {
-    Router::new().route("/:id", patch(handler::handler))
-}
