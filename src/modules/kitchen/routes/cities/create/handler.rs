27d532e87a677235c790f585aa4769fc7442b798
Adophilus <uchenna19of@gmail.com>
2025-04-15 10:49:05 +0100

    fix: corrected issue with the kitchen creation endpoint

--- src/modules/kitchen/routes/cities/create/handler.rs
+++ src/modules/kitchen/routes/cities/create/handler.rs
@@ -2,7 +2,7 @@ use super::service::service;
 use super::types::request;
 use crate::{modules::auth::middleware::AdminAuth, types::Context};
 use axum::{
-    extract::{Path, State},
+    extract::{Json, State},
     response::IntoResponse,
 };
 use std::sync::Arc;
@@ -10,7 +10,7 @@ use std::sync::Arc;
 pub async fn handler(
     _: AdminAuth,
     State(ctx): State<Arc<Context>>,
-    Path(payload): Path<request::Payload>,
+    Json(payload): Json<request::Payload>,
 ) -> impl IntoResponse {
     service(ctx, payload).await
 }
